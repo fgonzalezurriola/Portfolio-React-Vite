989225c10287586e0b7a7dbde2f78eab1e975156
Franzkier <140219655+fgonzalezurriola@users.noreply.github.com>
2024-10-22 22:13:57 -0300

    Styles: Updated Footer colors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,12 +5,13 @@ const Footer: React.FC = () => {
   const { t } = useTranslation();
 
   return (
-    <footer className="bg-gray-800 text-white py-6">
-      <div className="container mx-auto px-6 text-center">
+    <footer className="bg-gray-800 text-white py-6 border-t-4 border-gray-100 dark:border-gray-300">
+      <div className="container mx-auto px-6 text-center ">
         <p>{t('footer.copyright')}</p>
       </div>
     </footer>
   );
 };
+
 
 export default Footer;
