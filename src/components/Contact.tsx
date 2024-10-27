57eb2f8763824404c62c1ea1caedb424b22dc764
Franzkier <140219655+fgonzalezurriola@users.noreply.github.com>
2024-10-27 20:09:20 -0300

    Refactor: hide email from email grabbers with ts

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,23 +12,27 @@ const Contact: React.FC = () => {
     setEmail(`${username}@${domain}`);
   }, []);
 
+  const handleEmailClick = () => {
+    window.location.href = `mailto:${email}`;
+  };
+
   return (
     <section id="contact" className="py-10 bg-gray-100 dark:bg-gray-800">
       <div className="container mx-auto px-6">
         <h2 className="text-3xl font-bold text-center mb-8">{t('contact.title')}</h2>
         <div className="flex justify-center space-x-8">
           {/* Gmail */}
-          <a href={`mailto:${email}`} target="_blank" rel="noreferrer noopener"  className="text-gray-700 dark:text-gray-300 hover:text-blue-500 transition duration-300 flex flex-col items-center">
+          <button onClick={handleEmailClick} className="text-gray-700 dark:text-gray-300 hover:text-blue-500 transition duration-300 flex flex-col items-center">
             <FaEnvelope className="text-5xl" />
             <p className="text-center mt-2">{t('contact.email')}</p>
-          </a>
+          </button>
           {/* LinkedIn */}
           <a href="https://www.linkedin.com/in/fgonzalezurriola/" target="_blank" rel="noreferrer noopener" className="text-gray-700 dark:text-gray-300 hover:text-blue-500 transition duration-300 flex flex-col items-center">
             <FaLinkedin className="text-5xl ml-2" />
             <p className="text-center mt-2 ml-2">LinkedIn</p>
           </a>
           {/* Telegram */}
-          <a href="https://t.me/fgonzalezurriola" target="_blank" rel="noreferrer noopener"  className="text-gray-700 dark:text-gray-300 hover:text-blue-500 transition duration-300 flex flex-col items-center">
+          <a href="https://t.me/fgonzalezurriola" target="_blank" rel="noreferrer noopener" className="text-gray-700 dark:text-gray-300 hover:text-blue-500 transition duration-300 flex flex-col items-center">
             <FaTelegram className="text-5xl mr-0" />
             <p className="text-center mt-2">Telegram</p>
           </a>
