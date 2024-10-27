6d9f4f7762bc1946ab6535d89b80f9d083cfb726
Franzkier <140219655+fgonzalezurriola@users.noreply.github.com>
2024-10-27 19:18:11 -0300

    Chore: Updated text about updated project

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { FaLinkedin, FaTelegram, FaEnvelope } from 'react-icons/fa';
 
 const Contact: React.FC = () => {
   const { t } = useTranslation();
+  const [email, setEmail] = useState<string | null>(null);
+
+  useEffect(() => {
+    const username = "fgonzalezurriola";
+    const domain = "gmail.com";
+    setEmail(`${username}@${domain}`);
+  }, []);
 
   return (
     <section id="contact" className="py-10 bg-gray-100 dark:bg-gray-800">
@@ -11,7 +18,7 @@ const Contact: React.FC = () => {
         <h2 className="text-3xl font-bold text-center mb-8">{t('contact.title')}</h2>
         <div className="flex justify-center space-x-8">
           {/* Gmail */}
-          <a href="mailto:fgonzalezurriola@gmail.com" target="_blank" rel="noreferrer noopener"  className="text-gray-700 dark:text-gray-300 hover:text-blue-500 transition duration-300 flex flex-col items-center">
+          <a href={`mailto:${email}`} target="_blank" rel="noreferrer noopener"  className="text-gray-700 dark:text-gray-300 hover:text-blue-500 transition duration-300 flex flex-col items-center">
             <FaEnvelope className="text-5xl" />
             <p className="text-center mt-2">{t('contact.email')}</p>
           </a>
