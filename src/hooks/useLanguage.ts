ae8d19a3f59a8e1bdb6443258851c6d8ba6848f6
Franzkier <140219655+fgonzalezurriola@users.noreply.github.com>
2024-12-05 20:48:22 -0300

    Feat: added getting the preferred language by the user at first visit to display that language

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -4,6 +4,13 @@ import { useTranslation } from 'react-i18next';
 export const useLanguage = () => {
   const { i18n } = useTranslation();
 
+  const getSystemLanguagePreference = (): string => {
+    if (typeof window !== 'undefined') {
+      return window.navigator.language.split('-')[0];
+    }
+    return 'es';
+  }
+
   const toggleLanguage = () => {
     const newLanguage = i18n.language === 'en' ? 'es' : 'en';
     i18n.changeLanguage(newLanguage);
@@ -12,10 +19,16 @@ export const useLanguage = () => {
 
   useEffect(() => {
     const savedLanguage = localStorage.getItem('language');
-    if (savedLanguage && savedLanguage !== i18n.language) {
+    
+    if (savedLanguage === null) {
+      const systemLanguage = getSystemLanguagePreference();
+      i18n.changeLanguage(systemLanguage);
+      localStorage.setItem('language', systemLanguage);
+    } else {
       i18n.changeLanguage(savedLanguage);
     }
-  }, [i18n]);
+    
+  },[i18n])
 
   return { toggleLanguage };
-};
+}
