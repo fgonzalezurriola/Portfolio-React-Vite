d2796e1cee6d9746c23cf97943241857f1d5378c
Franzkier <140219655+fgonzalezurriola@users.noreply.github.com>
2024-12-13 19:10:09 -0300

    Feat: defined Experience html semantic and const to work

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { AiOutlineUsergroupAdd } from "react-icons/ai";
 
 const Experience: React.FC = () => {
   const { t } = useTranslation();
 
+  const Experience = [
+    {
+      icon: AiOutlineUsergroupAdd,
+      title: 'experience.experience1.title',
+      Company: 'experience.experience1.Company',
+      date: 'experience.experience1.date',
+      description: 'experience.experience1.description',
+    }
+  ]
+
   return (
-    <div>
-      <h2>{t('experience.title')}</h2>
-      <h3>{t('experience.Company')}</h3>
-      <h4>{t('experience.date')}</h4>
-      <p>{t('experience.description')}</p>
-    </div>
+    <section>
+      <div>
+        <h2 className="text-3xl font-bold text-center mb-12">
+          {t('experience.title')}
+        </h2>
+        <div className="p-6 flex flex-col h-full">
+          {Experience.map((experience, index) => (
+            <article key={index}>
+              <h3>
+                {t(experience.title)}
+              </h3>
+              <div>
+                <p>{t(experience.Company)}</p>
+                <p>{t(experience.date)}</p>
+                <p>{t(experience.description)}</p>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
