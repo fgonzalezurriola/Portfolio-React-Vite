ce3343e8a4f8fadcda71f316a5a822707d882118
Franzkier <140219655+fgonzalezurriola@users.noreply.github.com>
2025-01-29 23:06:39 -0300

    Refactor: added the json locale translations and connected to the component (!styles missing)

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,20 +5,20 @@ import { PiChalkboardTeacherLight } from "react-icons/pi";
 const Experience: React.FC = () => {
   const { t } = useTranslation();
 
-  const Experience = [
+  const experiences = [
     {
       icon: AiOutlineUsergroupAdd,
-      title: "experience.experience1.title",
-      company: "experience.experience1.Company",
-      date: "experience.experience1.date",
-      description: "experience.experience1.description",
+      title: "experiences.experience1.title",
+      company: "experiences.experience1.company",
+      date: "experiences.experience1.date",
+      description: "experiences.experience1.description",
     },
     {
       icon: PiChalkboardTeacherLight,
-      title: "experience.experience2.title",
-      company: "experience.experience2.Company",
-      date: "experience.experience2.date",
-      description: "experience.experience2.description",
+      title: "experiences.experience2.title",
+      company: "experiences.experience2.company",
+      date: "experiences.experience2.date",
+      description: "experiences.experience2.description",
     },
   ];
 
@@ -26,10 +26,10 @@ const Experience: React.FC = () => {
     <section className="py-12 transition-color">
       <div className="container mx-auto px-6">
         <h2 className="text-3xl md:text-5xl font-bold text-center py-6 mb-6">
-          {t("Experience.title")}
+          {t("experiences.title")}
         </h2>
         <div>
-          {Experience.map((experience, index) => (
+          {experiences.map((experience, index) => (
             <article key={index} className="">
               <div className="p-6 flex-grow">
                 <experience.icon size={48} className="text-blue-500 mb-4" />
